01a4cde91801d3f641e5124eb82b217a9c432120
Paola Toloza Cravero <paotoloza@MacBook-Air-de-Pao-Toloza.local>
2022-07-20 19:14:17 -0400

    Clase 03

--- src/ClassState.js
+++ src/ClassState.js
@@ -2,15 +2,31 @@ import React from 'react';
 
 //crear un componente de tipo clase
  class ClassState extends React.Component { //ClassState hereda todo lo que trae React.Component
+    constructor(props) {
+        super(props); //permite ir actializando el estado
+   
+        this.state = { //crear un estado//
+          error: true,
+        };
+      }
+   
    render() {
      return (
        <div>
-         <h2>Eliminar ClassState</h2>
+          <h2>Eliminar {this.props.name}</h2>
 
          <p>Por favor, escribe el código de seguridad.</p>
 
+         {this.state.error && (
+           <p>Error: el código es incorrecto</p>
+         )}
+
          <input placeholder="Código de seguridad" />
-         <button>Comprobar</button>
+         <button
+           onClick={() =>
+             this.setState(prevState => ({ error: !prevState.error })) //si se presiona el botón error pasa a ser false (no se ve en pantalla error)
+           }
+         >Comprobar</button>
        </div>
      );
    }
--- src/UseState.js
+++ src/UseState.js
@@ -1,15 +1,23 @@
 import React from 'react';
 
 //crear una funcion con reacthook
- function UseState() {
+function UseState({ name }) {
+    const [error, setError] = React.useState(true); //crear un estado//
+ 
    return (
      <div>
-       <h2>Eliminar UseState</h2>
+       <h2>Eliminar {name}</h2>
 
        <p>Por favor, escribe el código de seguridad.</p>
 
+       {error && (
+         <p>Error: el código es incorrecto</p>
+       )}
+
        <input placeholder="Código de seguridad" />
-       <button>Comprobar</button>
+        <button
+         onClick={() => setError(!error)} //si se presiona el botón error pasa a ser false (no se ve en pantalla error)
+       >Comprobar</button>
      </div>
    );
  }
