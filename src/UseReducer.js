07e6ac0e4bfd65f17c465996f13f914802a1699f
Paola Toloza Cravero <paotoloza@MacBook-Air-de-Pao-Toloza.local>
2022-07-28 16:42:06 -0400

    Clase 15

--- src/UseReducer.js
+++ src/UseReducer.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
- const SECURITY_CODE = 'paradigma';
+ const SECURITY_CODE = 'paradigma'; //contraseña
 
  function UseReducer({ name }) {
     const [state, dispatch] = React.useReducer(reducer, initialState);
+
+    const onConfirm = () => dispatch({ type: actionTypes.confirm });
+    const onError = () => dispatch({ type: actionTypes.error });
+    const onCheck = () => dispatch({ type: actionTypes.check });
+    const onDelete = () => dispatch({ type: actionTypes.delete });
+    const onReset = () => dispatch({ type: actionTypes.reset });
+ 
+    const onWrite = ({ target: { value } }) => {
+      dispatch({ type: actionTypes.write, payload: value });
+    }
  
     React.useEffect(() => {
         console.log("Empezando el efecto")
@@ -13,9 +23,9 @@ import React from 'react';
             console.log("Haciendo la validación")
    
             if (state.value === SECURITY_CODE) { //verificando código
-              dispatch({ type: 'CONFIRM' }); // código correcto
+                onConfirm(); // código correcto
             } else { 
-              dispatch({ type: 'ERROR' }); //código incorrecto
+                onError(); //código incorrecto
             }
    
             console.log("terminando la validación")
@@ -42,18 +52,13 @@ import React from 'react';
             <input
               placeholder="Código de seguridad"
               value={state.value} //código escrito por usuario
-              onChange={(event) => {
-                dispatch({ type: 'WRITE', payload: event.target.value });
-                // onWrite(event.target.value);
-              }}
+              onChange={onWrite}
             />
             <button //botón para confirmar
-              onClick={() => {
-                dispatch({ type: 'CHECK' });
-                // onCheck();
-              }}
-            >Comprobar</button>
-          </div>
+              onClick={onCheck}>
+              Comprobar
+            </button>
+            </div>
         );
       } else if (!!state.confirmed && !state.deleted) { // estado confirmado en verdader y deleted en falso
         return (
@@ -61,19 +66,11 @@ import React from 'react';
             <p>Pedimos confirmación. ¿Tas segurx?</p> {/*pregunta para eliminar*/}
    
             <button //botón para eliminar
-              onClick={() => {
-                dispatch({ type: 'DELETE' });
-                // onDelete();
-              }}
-            >
+              onClick={onDelete}>
               Sí, eliminar
             </button>
             <button //botón de no eliminar
-              onClick={() => {
-                dispatch({ type: 'RESET' });
-                // onReset();
-              }}
-            >
+              onClick={onReset}>
               Nop, me arrepentí
             </button>
           </React.Fragment>
@@ -84,11 +81,7 @@ import React from 'react';
               <p>Eliminado con éxito</p>
   
               <button //botón para reseatear y volver al inicio
-           onClick={() => {
-             dispatch({ type: 'RESET' });
-             // onReset();
-           }}
-         >
+          onClick={onReset}>
            Resetear, volver atrás
          </button>
        </React.Fragment>
@@ -97,38 +90,46 @@ import React from 'react';
  }
 
  const initialState = { //estado inicial
-   value: 'paradigma',
+    value: '',
    error: false,
    loading: false,
    deleted: false,
    confirmed: false,
  };
  
+ const actionTypes = { //objeto con el nombre de las variables
+    confirm: 'CONFIRM',
+    delete: 'DELETE',
+    error: 'ERROR',
+    write: 'WRITE',
+    reset: 'RESET',
+  };
+
  const reducerObject = (state, payload) => ({
-    'CONFIRM': { //confirmación
+     [actionTypes.confirm]: { //confirmación
       ...state,
       error: false,
       loading: false,
       confirmed: true,
     },
-    'ERROR': { //error
+    [actionTypes.error]: { //error
       ...state,
       error: true,
       loading: false,
     },
-    'WRITE': { //nuevo código escrito por usuario
+    [actionTypes.write]: {//nuevo código escrito por usuario
       ...state,
       value: payload
     },
-    'CHECK': { //cargando
+    [actionTypes.check]: {//cargando
       ...state,
       loading: true,
     },
-    'DELETE': { //borrar
+    [actionTypes.delete]: {//borrar
       ...state,
       deleted: true,
     },
-    'RESET': { //resetear y volver al inicio
+    [actionTypes.reset]: {//resetear y volver al inicio
       ...state,
       confirmed: false,
       deleted: false,
